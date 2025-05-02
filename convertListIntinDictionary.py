89516f3ddd9a36c7723cc3395eb3305ed7a8d2c4
ANKANA-MONDAL-MCA-A-2024-26-09-UEMK <140162922+ANKANA-MONDAL-MCA-A-2024-26-09-UEMK@users.noreply.github.com>
2025-05-02 07:38:27 +0530

    02/05/2025

--- convertListIntinDictionary.py
+++ convertListIntinDictionary.py
@@ -4,6 +4,6 @@ def list_to_dict(lst):
     return {i: lst[i] for i in range(len(lst))}
 
 # Example Usage
-lst = ['apple', 'banana', 'cherry']
+lst = ['apple', 'bannana', 'cherry']
 result = list_to_dict(lst)
 print(result)
